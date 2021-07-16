39e6ce097a181906fa6f2ab214c67f103f2fdbcc
User <andikajayawiguna@gmail.com>
2021-07-16 21:58:41 +0800

    Structuring the folder and implement Views

--- core/Router.php
+++ core/Router.php
@@ -24,10 +24,22 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if($callback == false) {
-            echo "Not Found!";
-            exit;
+            return "Not Found!";
         }
-        echo call_user_func($callback);
-        
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        include_once __DIR__."/../views/$view.php";
+    }
+
+    public function layoutContent()
+    {
+        # code...
     }
 }
