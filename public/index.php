19733a5e81c603e2c27cca7decb913de90d02651
User <andikajayawiguna@gmail.com>
2021-07-16 23:24:05 +0800

    Implement Controllers

--- public/index.php
+++ public/index.php
@@ -2,14 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use app\controllers\SiteController;
 use app\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-$app->router->get('/contact', 'contact');
-$app->router->post('/contact', function() {
-    return 'Handling Submitted Data';
-});
+$app->router->get('/', [new SiteController, 'home']);
+$app->router->get('/contact', [new SiteController, 'contact']);
+$app->router->post('/contact', [new SiteController, 'handleContact']);
 
 $app->run();
