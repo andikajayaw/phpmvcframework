83bd2954aa02b727572a48c6af492ac460e56905
User <andikajayawiguna@gmail.com>
2021-07-17 23:19:04 +0800

    Create Login&Register Page

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use app\controllers\SiteController;
+use app\controllers\AuthController;
 use app\core\Application;
 
 $app = new Application(dirname(__DIR__));
@@ -10,5 +11,11 @@ $app = new Application(dirname(__DIR__));
 $app->router->get('/', [new SiteController, 'home']);
 $app->router->get('/contact', [new SiteController, 'contact']);
 $app->router->post('/contact', [new SiteController, 'handleContact']);
+
+$app->router->get('/login', [new AuthController, 'login']);
+$app->router->post('/login', [new AuthController, 'login']);
+
+$app->router->get('/register', [new AuthController, 'register']);
+$app->router->post('/register', [new AuthController, 'register']);
 
 $app->run();
---
+++ views/register.php
@@ -0,0 +1,35 @@
+<h1>Register</h1>
+
+<form action="" method="POST">
+    <div class="row">
+        <div class="col">
+            <div class="form-group">
+                <label>First Name</label>
+                <input type="text" class="form-control" name="firstname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label>Last Name</label>
+                <input type="text" class="form-control" name="lastname">
+            </div>  
+        </div>
+    </div>
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" name="email">
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" name="password">
+  </div>
+  <div class="form-group">
+    <label>Confirm Password</label>
+    <input type="password" class="form-control" name="confirmpassword">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
