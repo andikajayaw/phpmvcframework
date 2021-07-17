df075e2b9bfe2e3837c6bd3e3cfa9d54b50b37bd
User <andikajayawiguna@gmail.com>
2021-07-17 23:40:12 +0800

    Implement second layout

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,11 +8,13 @@ class AuthController extends Controller {
 
     public function login()
     {
+        $this->setLayout('auth');
         return $this->render('login');
     }
 
     public function register(Request $request)
     {
+        $this->setLayout('auth');
         if($request->isPost()) {
             return 'Handle submitted data';
         }
--- core/Application.php
+++ core/Application.php
@@ -9,6 +9,7 @@ class Application
     public Request $request;
     public Response $response;
     public static Application $app;
+    public Controller $controller;
     public function __construct($rootPath)
     {
         self::$ROOT_DIR = $rootPath;
@@ -16,10 +17,21 @@ class Application
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->controller = new Controller();
     }
 
     public function run()
     {
        echo $this->router->resolve();
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function setController(Controller $controller)
+    {
+        $this->controller = $controller;
     }
 }
